1b5176fbcb6306e3b50afa9087056e211686a039
kenziNakagawa <naka9218054@gmail.com>
2026-08-28 13:58:54 +0900

    ３

--- db.php
+++ db.php
@@ -37,8 +37,8 @@ function getDBConnection(): PDO {
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$charset}"
+        PDO::ATTR_EMULATE_PREPARES   => false
+        // PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$charset}"
     ];
 
     try {

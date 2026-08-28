71f3af13987b839b5989326608c3c26c571b5964
kenziNakagawa <naka9218054@gmail.com>
2026-08-28 13:09:51 +0900

    6

--- config.php
+++ config.php
@@ -10,7 +10,7 @@
  */
 
 return [
-    'host'     => 'mysql3117.sakura.ne.jp',
+    'host'     => 'mysql80.abot.sakura.ne.jp',
     // 'port'     => '3306',
     'dbname'   => 'abot_todolist',
     'user'     => 'abot_todolist',

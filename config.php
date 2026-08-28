98bae38e9c0f317cb538f213aed422682de5e210
kenziNakagawa <naka9218054@gmail.com>
2026-08-28 13:17:20 +0900

    7

--- config.php
+++ config.php
@@ -3,17 +3,20 @@
  * Database Configuration
  * MySQL connection settings for ToDoList 
  * データ保存先：さくらデータベース abot_db  
- * 'host'     => 'localhost',
- * 'dbname'   => 'todolist_app',
- * 'user'     => 'kenzi',
- * 'password' => 'Kenzi_1732',
+
  */
 
 return [
+    // 'host'     => 'localhost',
+    // 'dbname'   => 'todolist_app',
+    // 'user'     => 'kenzi',
+    // 'password' => 'Kenzi_1732',
+    // ------------
     'host'     => 'mysql80.abot.sakura.ne.jp',
-    // 'port'     => '3306',
-    'dbname'   => 'abot_todolist',
+    'dbname'   => 'mysql80',
     'user'     => 'abot_todolist',
     'password' => 'Kenzi_1732',
-    // 'charset'  => 'utf8mb4'
+    // -----------
+    'port'     => '3306',
+    'charset'  => 'utf8mb4'
 ];
